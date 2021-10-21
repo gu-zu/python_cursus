afe4171fd2b6c3db98d2ce8e19595560550183a0
gu-zu <77447883+gu-zu@users.noreply.github.com>
2021-10-21 14:27:40 +0200

    opdr5

---
+++ opdrachten/opdr5.py
@@ -0,0 +1,14 @@
+# comments gebasseerd op originele waarden voor nr 1 en nr 2:
+# nr1=5
+# nr2=4
+
+nr1 = 11
+nr2 = 10
+nr3 = 5
+print(nr3 / (nr1 % nr2))  # 5 / 1
+nr1 = nr1 + 1  # 6
+print(nr3 / (nr1 % nr2))  # 5 / 2
+nr1 = nr1 + 1  # 7
+print(nr3 / (nr1 % nr2))  # 5 / 3
+nr1 = nr1 + 1  # 8
+print(nr3 / (nr1 % nr2))  # 5 / 0 (8%4=0)
