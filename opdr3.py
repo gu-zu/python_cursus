476796ca15250f9b19813fe73381da7dbefbe14f
gu-zu <77447883+gu-zu@users.noreply.github.com>
2021-10-21 10:36:17 +0200

    finished opdr3

--- opdr3.py
+++ opdr3.py
@@ -1,4 +1,7 @@
 prijs = 24.95
-inkoop = prijs*0.6
+inkoop = prijs * 0.6
+verzending = 0.75  # 3 voor eerste
+winst = prijs - inkoop - verzending
+verkocht = 60
 
-winst = prijs-inkoop
+print(winst * verkocht - (3 - verzending))
