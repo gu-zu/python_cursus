f72861d62f587ab4df8cbcfecc877e13c63ac8a0
gu-zu <77447883+gu-zu@users.noreply.github.com>
2021-10-29 12:51:36 +0200

    added 11+12

---
+++ opdrachten/opdr11.py
@@ -0,0 +1,13 @@
+print("1. ", 2 < 5)  # true 2 is minder dan 5
+print("2. ", 2 <= 5)  # true
+print("3. ", 3 > 3)  # false; gelijk aan != meer dan
+print("4. ", 3 >= 3)  # true
+print("5. ", 3 == 3.0)  # true
+print("6. ", 3 == "3")  # string en int zijn niet gelijk
+print("7. ", "syntax" == "syntax")  # true
+print("8. ", "syntax" == "semantiek")  # false; andere string
+print("9. ", "syntax" == " syntax")  # false; spatie telt ook mee
+print("10. ", "Python" != "rommel")  # true
+print("11. ", "Python" > "Perl")  # true
+print("12. ", "banaan" < "mango")  # true
+print("13. ", "banaan" < "Mango")  # false
---
+++ opdrachten/opdr12.py
@@ -0,0 +1,6 @@
+print("y" in "Python")  # true; y zit in Python
+print("x" in "Python")  # false; x zit niet in Python
+print("p" in "Python")  # false; hoofdletter is geen kleine letter
+print("th" in "Python")  # true
+print("to" in "Python")  # false; andere volgorde
+print("y" not in "Python")  # false
